f49d4500aeea48ac64d04d35e7ba9105a27c7b94
luluwaffless <lcsgoes210@gmail.com>
2025-01-09 22:27:40 -0300

    2.0.0

--- index.js
+++ index.js
@@ -1,52 +1,66 @@
 import { readFileSync, writeFileSync, appendFileSync } from "node:fs";
 import axios from "axios";
 import providers from "./providers.js";
+import { Client, GatewayIntentBits, EmbedBuilder } from 'discord.js';
 import dotenv from "dotenv";
 dotenv.config();
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 const intl = new Intl.NumberFormat("pt-BR", { style: "currency", currency: "BRL" });
 const items = JSON.parse(readFileSync("items.json", "utf8"));
 const saveItems = () => writeFileSync("items.json", JSON.stringify(items));
 const log = (data, error) => appendFileSync(`${error ? "errors" : "logs"}.txt`, `[${new Date().toISOString()}] ${data}\n`);
-const send = (data) => axios.post(process.env.webhook, data);
-function check() {
+const send = (channel, data) => channel.send(data).catch(err => {
+    log(`❌ Error sending message: ${err.message}, ${err.stack || 'no stack trace available'}`, true);
+});
+let category;
+let channels = {};
+const getChannel = async (id) => {
+    if (channels[id]) return channels[id];
+    channels[id] = await client.channels.fetch(id);
+    return channels[id];
+};
+const createChannel = async (name) => {
+    const newChannel = await category.children.create({
+        name: name,
+        type: 0,
+    });
+    channels[newChannel.id] = newChannel;
+    return newChannel;
+};
+const nameify = (str, max) => str.toLowerCase().split(",")[0].normalize("NFD").replace(/[\u0300-\u036f]/g, '').replace(/[^a-zA-Z0-9]+/g, '-').replace(/^-+|-+$/g, '').slice(0, max);
+function checkChannels() {
+    return new Promise(async (r) => {
+        for (let item of items) {
+            if (!item.id) {
+                const nameStart = `${providers[item.provider].emoji}︱`;
+                const newChannel = await createChannel(nameStart + nameify(item.name, 100 - nameStart.length));
+                item.id = newChannel.id;
+                saveItems();
+            };
+        };
+        r();
+    });
+};
+function checkItems() {
     items.forEach(item => {
         axios.get(item.url)
-            .then((response) => {
+            .then(async (response) => {
                 const [vista, parc] = providers[item.provider].parser(response.data);
                 if (vista != item.vista && parc != item.parc) {
+                    if ((vista > 0 && parc == 0) || (vista == 0 && parc > 0)) return;
                     const diffVista = Math.abs(item.vista - vista);
                     const diffParc = Math.abs(item.parc - parc);
                     const vistaDiscount = vista < item.vista;
                     const parcDiscount = parc < item.parc;
-                    send({
-                        "content": "-# ||@everyone||",
-                        "embeds": [
-                            {
-                                "title": item.name,
-                                "url": item.url,
-                                "color": item.vista > 0 && item.parc > 0 ? (vista > 0 && parc > 0 ? (vistaDiscount && parcDiscount ? 0x00ff00 : 0xff0000) : 0x000000) : 0xffff00,
-                                "fields": [
-                                    {
-                                        "name": "Preço à vista",
-                                        "value": vista > 0 ? `${intl.format(vista)}${item.vista > 0 ? ` (${vistaDiscount ? "-" : "+"}${intl.format(diffVista)})` : ""}` : "Indisponível",
-                                        "inline": true
-                                    },
-                                    {
-                                        "name": "Preço parcelado",
-                                        "value": parc > 0 ? `${intl.format(parc)}${item.parc > 0 ? ` (${parcDiscount ? "-" : "+"}${intl.format(diffParc)})` : ""}` : "Indisponível",
-                                        "inline": true
-                                    }
-                                ],
-                                "author": {
-                                    "name": providers[item.provider].name,
-                                    "icon_url": providers[item.provider].icon
-                                },
-                                "image": {
-                                    "url": item.image
-                                }
-                            }
-                        ]
-                    });
+                    const channel = await getChannel(item.id);
+                    log(`🛒 ${item.name} ${item.vista && item.parc ? `mudou de preço. De ${intl.format(item.vista)} à vista ou ${intl.format(item.parc)} parcelando para ${vista && parc ? `${intl.format(vista)} à vista (${vistaDiscount ? "-" : "+"}${intl.format(diffVista)}) ou ${intl.format(parc)} parcelando (${parcDiscount ? "-" : "+"}${intl.format(diffParc)}).` : "indisponível."}` : `ficou disponível por ${intl.format(vista)} à vista (${vistaDiscount ? "-" : "+"}${intl.format(diffVista)}) ou ${intl.format(parc)} parcelando (${parcDiscount ? "-" : "+"}${intl.format(diffParc)}).`}`);
+                    send(channel, { content: "-# ||@everyone||", embeds: [ new EmbedBuilder()
+                        .setTitle(item.name)
+                        .setURL(item.url)
+                        .setColor(item.vista > 0 && item.parc > 0 ? (vista > 0 && parc > 0 ? (vistaDiscount && parcDiscount ? 0x00ff00 : 0xff0000) : 0x000000) : 0xffff00)
+                        .addFields({ name: "Preço à vista", value: vista > 0 ? `${intl.format(vista)}${item.vista > 0 ? ` (${vistaDiscount ? "-" : "+"}${intl.format(diffVista)})` : ""}` : "Indisponível", inline: true }, { name: "Preço parcelado", value: parc > 0 ? `${intl.format(parc)}${item.parc > 0 ? ` (${parcDiscount ? "-" : "+"}${intl.format(diffParc)})` : ""}` : "Indisponível", inline: true })
+                        .setAuthor({ name: providers[item.provider].name, iconURL: providers[item.provider].icon })
+                        .setImage(item.image) ]});
                     item.vista = vista;
                     item.parc = parc;
                     saveItems();
@@ -56,6 +70,35 @@ function check() {
                 log(`❌ Error fetching data: ${error.message}, ${error.stack || 'no stack trace available'}`, true);
             });
     });
-    setTimeout(check, Number(process.env.timeout) * 1000);
+    setTimeout(checkItems, Number(process.env.timeout) * 1000);
 };
-check();
+client.once("ready", async () => {
+    log("🟢 Online.");
+    console.log(`Logged in as ${client.user.tag}!`);
+    category = await client.channels.fetch(process.env.category);
+    if (!category || category.type !== 4) {
+        console.error('Invalid category ID. Please input one in .env and restart.');
+        return process.exit();
+    };
+    if (category.name != "online 🟢") await category.setName("online 🟢");
+    for (let evt of ['SIGTERM', 'SIGINT', 'SIGHUP']) {
+        process.on(evt, async function () {
+            process.stdin.resume();
+            log("🔴 Offline.");
+            if (category.name != "offline 🔴") await category.setName("offline 🔴");
+            process.exit();
+        });
+    };
+    process.on('unhandledRejection', (reason, promise) => {
+        sessionInfo.ce += 1;
+        log(`❌ Unhandled Rejection at ${promise}: ${reason} (${reason.message || 'no message'}, ${reason.stack || 'no stack'})`, true);
+    });
+    
+    process.on('uncaughtException', (err) => {
+        sessionInfo.ce += 1;
+        log(`❌ Uncaught Exception: ${err.message}, ${err.stack}`, true);
+    });
+    await checkChannels();
+    checkItems();
+});
+client.login(process.env.token);
--- providers.js
+++ providers.js
@@ -3,11 +3,25 @@ export default {
     kabum: {
         name: "KaBuM!",
         icon: "https://static.kabum.com.br/conteudo/favicon/favicon.ico",
+        emoji: "💥",
         parser: (b) => {
             const $ = load(b);
             const vista = $("#blocoValores > div.sc-a24aba34-3.hSVqxN > div.sc-a24aba34-1.cpLDBn > div > h4").text().split("R$")[1];
             const parc = $("#blocoValores > div.sc-4f698d6c-0.hkfkrb > b").text().split("R$")[1];
             return [ vista ? Number(vista.trim().replace(",", ".")) : 0, parc ? Number(parc.trim().replace(",", ".")) : 0 ];
+        }
+    },
+    mercadolivre: {
+        name: "Mercado Livre",
+        icon: "https://http2.mlstatic.com/frontend-assets/ml-web-navigation/ui-navigation/5.21.22/mercadolibre/180x180.png",
+        emoji: "🤝",
+        parser: (b) => {
+            const $ = load(b);
+            const vista = $("#price > div > div.ui-pdp-price__main-container > div > span:nth-child(1) > span").text().split("R$")[1];
+            const pps = $("#pricing_price_subtitle").text().split(" ");
+            const multiplier = pps[1] ? pps[1].split("x")[0] : 0;
+            const parc = pps[2] ? pps[2].split("R$")[1] : 0;
+            return [ vista ? Number(vista.trim().replace(",", ".")) : 0, parc && multiplier ? Number(parc.trim().replace(",", ".")) * Number(multiplier) : 0 ];
         }
     }
 };
